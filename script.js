57adb42295a043ab43fc98cd4ac28fdff68d36d8
jenish jain <jenishjain@rocketmail.com>
2019-12-24 13:30:19 +0530

    first commit to github

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const hostURL = "https://nameless-wave-18089.herokuapp.com/";
-// const hostURL = "https://localhost:3000/"
+// const hostURL = "https://localhost:3000/" // local host
+const mainTable = document.getElementById('main-table');
+const incomeTable = document.getElementById('income-table');
+const expenseTable = document.getElementById('expense-table');
 
 async function fetchTransaction(projectId =" ",type=" ") {   // return todo data object
-    //   console.log('fetching all todos');
     let res = await fetch(hostURL +"api/transaction?"+"projectId="+projectId.toString()+ "&type=" + type);
     let data = await res.json();
     // console.log(data);
@@ -10,9 +12,23 @@ async function fetchTransaction(projectId =" ",type=" ") {   // return todo data
   }
 
 async function displayTransaction(){
-  let transactionData = await fetchTransaction(12345,"income");
+  let transactionData = await fetchTransaction(12345);
   let transactionList = transactionData.data;
   transactionList.forEach(createTransaction);
+}
+
+async function displayIncome(){
+  console.log("income requested");
+  let transactionData = await fetchTransaction(12345,"income");
+  let transactionList = transactionData.data;
+  transactionList.forEach(createIncome);
+}
+
+async function displayExpense(){
+  console.log("expense requested");
+  let transactionData = await fetchTransaction(12345,"expense");
+  let transactionList = transactionData.data;
+  transactionList.forEach(createExpense);
 }
 
 async function createTransaction(transaction){
@@ -42,7 +58,55 @@ async function createTransaction(transaction){
     table.appendChild(row);
 }
 
+async function createIncome(transaction){
+
+  let table = document.getElementById("income-table");
+  let row = document.createElement('tr');
+  let source = document.createElement('td');
+  source.innerText = transaction.source;
+  let description = document.createElement('td');
+  description.innerText = transaction.description;
+  let type = document.createElement('td');
+  type.innerText = transaction.type;
+  let amount = document.createElement('td');
+  amount.innerText = transaction.amount + "  ₹";
+  let timestamp = document.createElement('td');
+  amount.classList.add("income");
+  timestamp.innerText = transaction.timestamp;
+  row.append(source);
+  row.appendChild(description);
+  row.appendChild(type);
+  row.appendChild(amount);
+  row.appendChild(timestamp);
+  table.appendChild(row);
+}
+
+async function createExpense(transaction){
+
+  let table = document.getElementById("expense-table");
+  let row = document.createElement('tr');
+  let source = document.createElement('td');
+  source.innerText = transaction.source;
+  let description = document.createElement('td');
+  description.innerText = transaction.description;
+  let type = document.createElement('td');
+  type.innerText = transaction.type;
+  let amount = document.createElement('td');
+  amount.innerText = transaction.amount + "  ₹";
+  let timestamp = document.createElement('td');
+  amount.classList.add("expense");
+  timestamp.innerText = transaction.timestamp;
+  row.append(source);
+  row.appendChild(description);
+  row.appendChild(type);
+  row.appendChild(amount);
+  row.appendChild(timestamp);
+  table.appendChild(row);
+}
+
 displayTransaction();
+displayIncome();
+displayExpense();
 
 
 async function cashInHand(projectId){
@@ -62,10 +126,35 @@ async function cashInHand(projectId){
     }
   })
   cash = income - expense;
-  document.getElementById("cash-in-hand").innerText = cash.toString();
+  let profitPer = ((cash/income)*100).toFixed(2);
+  document.getElementById("balance").innerText = cash.toString() + "  ₹" ;
   document.getElementById("cash-bar").value = cash.toString();
   document.getElementById("cash-bar").max = income.toString();
+  document.getElementById("profit").innerText = profitPer.toString() + " %";
+  document.getElementById("totalIncome").innerText = income.toString()+ "  ₹" ;
+  document.getElementById("totalExpense").innerText = expense.toString()+ "  ₹" ;
+
   return cash;
 }
 
 cashInHand("12345");
+
+const sliderBtn = document.getElementById("btn-slider");
+function showAll(){
+  mainTable.style.left="0vw"
+  incomeTable.style.left ="100vw"
+  expenseTable.style.left="200vw"
+  sliderBtn.style.left ="0px"
+}
+function showIncome(){
+  mainTable.style.left="-100vw"
+  incomeTable.style.left ="0vw"
+  expenseTable.style.left = "100vw"
+  sliderBtn.style.left ="90px"
+}
+function showExpense(){
+  mainTable.style.left="-200vw"
+  incomeTable.style.left="-100vw"
+  expenseTable.style.left ="0vw"
+  sliderBtn.style.left ="200px"
+}
