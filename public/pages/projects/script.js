47df47d59d8b54d2fc1a0cd48d0a79f13b26c609
jenish jain <jenishjain@rocketmail.com>
2020-01-07 19:20:12 +0530

    corrections

--- public/pages/projects/script.js
+++ public/pages/projects/script.js
@@ -1,5 +1,5 @@
-// const hostURL = "https://nameless-wave-18089.herokuapp.com/";
-const hostURL = "http://localhost:3000/";
+const hostURL = "https://nameless-wave-18089.herokuapp.com/";
+// const hostURL = "http://localhost:3000/";
 
 async function fetchUserDetails(){
     let res = await fetch(hostURL+ "api/user/dashboard");
