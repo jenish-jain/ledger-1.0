3fe408b1b23bdf856eb818c6bef0f8afcb7dff44
jenish jain <jenishjain@rocketmail.com>
2020-01-09 20:27:02 +0530

    asdf

--- public/pages/projects/script.js
+++ public/pages/projects/script.js
@@ -2,7 +2,12 @@ const hostURL = "https://nameless-wave-18089.herokuapp.com/";
 // const hostURL = "http://localhost:3000/";
 
 async function fetchUserDetails(){
-    let res = await fetch(hostURL + "api/user/dashboard",{redirect: 'follow'});
+    let cookie = document.cookie();
+    let res = await fetch(hostURL + "api/user/dashboard",
+    {redirect: 'follow',
+     headers:{
+        "Cookie":cookie
+      }});
     // let userJson = await res.json();
     console.log(res);
     // console.log(userJson);
